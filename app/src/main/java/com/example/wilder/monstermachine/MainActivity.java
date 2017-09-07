0dd8bd7500a3aaba429e034a22f8f2273176cbca
KekeTSC <kelian.daste31@gmail.com>
2017-09-07 17:04:12 +0200

    Interface graphique premiere page

--- app/src/main/java/com/example/wilder/monstermachine/MainActivity.java
+++ app/src/main/java/com/example/wilder/monstermachine/MainActivity.java
@@ -1,29 +1,37 @@
 package com.example.wilder.monstermachine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity  {
-
-    private ListView listView;
-
-    private String[] monsters = new String[]{
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton btnSealion = (ImageButton) findViewById(R.id.imageSealion);
+        ImageButton btnChocoBunny = (ImageButton) findViewById(R.id.imageChocoBunny);
+        ImageButton btnPandaClaus = (ImageButton) findViewById(R.id.imagePandaClaus);
+        ImageButton btnMetaselash = (ImageButton) findViewById(R.id.imageMetaselash);
+        ImageButton btnMechamancer = (ImageButton) findViewById(R.id.imageMechamancer);
+        ImageButton btnKomocat = (ImageButton) findViewById(R.id.imageKomocat);
+        ImageButton btnFenix = (ImageButton) findViewById(R.id.imageFenix);
+        ImageButton btnThunderEagle = (ImageButton) findViewById(R.id.imageThunderEagle);
+        ImageButton btnPegasus = (ImageButton) findViewById(R.id.imagePegasus);
+        ImageButton btnObsidia = (ImageButton) findViewById(R.id.imageObsidia);
 
-        monsters = getResources().getStringArray(R.array.monsters_array);
-        listView = (ListView) findViewById(R.id.monsterList);
+        btnSealion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
-                R.layout.liste_monstres, monsters);
-        listView.setAdapter(adapter);
     }
 
 }
