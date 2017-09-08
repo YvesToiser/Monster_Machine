25e34aad2012b06d197604befa38a971ad21a2c0
Yves Toiser <yves.toiser@gmail.com>
2017-09-08 11:38:41 +0200

    Creation des 10 boutons et assignation des données en fonction de l'id passé depuis la page d'accueil

--- app/src/main/java/com/example/wilder/monstermachine/DetailActivity.java
+++ app/src/main/java/com/example/wilder/monstermachine/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.example.wilder.monstermachine;;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class DetailActivity extends AppCompatActivity {
@@ -16,19 +20,148 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
 
+        // Hide action bar
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
+
+        // Get id from MainActivity
+        int monsterId ;
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if(bundle != null){
+            monsterId = bundle.getInt("monsterId");
+        }else{monsterId=0;}
+
+        // Define all the dynamic data
+        // images
+        ImageView evol0 = (ImageView) findViewById(R.id.evolution_0);
+        ImageView evol1 = (ImageView) findViewById(R.id.evolution_1);
+        ImageView evol2 = (ImageView) findViewById(R.id.evolution_2);
+        ImageView evol3 = (ImageView) findViewById(R.id.evolution_3);
+        ImageView elmt1 = (ImageView) findViewById(R.id.element_1);
+        ImageView elmt2 = (ImageView) findViewById(R.id.element_2);
+        ImageView skill = (ImageView) findViewById(R.id.skill);
+
+
+        // Monster Name
+        TextView nomMonstre = (TextView) findViewById(R.id.Nom_monstre);
+
+
+        // Set values
+
+        if(monsterId==0){
+            evol0.setImageResource(R.drawable.sealion_0);
+            evol1.setImageResource(R.drawable.sealion_1);
+            evol2.setImageResource(R.drawable.sealion_2);
+            evol3.setImageResource(R.drawable.sealion_3);
+            elmt1.setImageResource(R.drawable.bte_fire);
+            elmt2.setImageResource(R.drawable.bte_water);
+            skill.setImageResource(R.drawable.skill_sealion);
+            nomMonstre.setText("Sealion");
+
+        }else if(monsterId==1){
+            evol0.setImageResource(R.drawable.chocobunny_0);
+            evol1.setImageResource(R.drawable.chocobunny_1);
+            evol2.setImageResource(R.drawable.chocobunny_2);
+            evol3.setImageResource(R.drawable.chocobunny_3);
+            elmt1.setImageResource(R.drawable.bte_dark);
+            elmt2.setImageResource(R.drawable.bte_magic);
+            skill.setImageResource(R.drawable.skill_chocobunny);
+            nomMonstre.setText("Choco Bunny");
+
+        }else if(monsterId==2){
+            evol0.setImageResource(R.drawable.panda_claus_0);
+            evol1.setImageResource(R.drawable.panda_claus_1);
+            evol2.setImageResource(R.drawable.panda_claus_2);
+            evol3.setImageResource(R.drawable.panda_claus_3);
+            elmt1.setImageResource(R.drawable.bte_magic);
+            elmt2.setImageResource(R.drawable.bte_legend);
+            skill.setImageResource(R.drawable.skill_panda_claus);
+            nomMonstre.setText("Panda Claus");
+
+        }else if(monsterId==3){
+            evol0.setImageResource(R.drawable.metaselash_0);
+            evol1.setImageResource(R.drawable.metaselash_1);
+            evol2.setImageResource(R.drawable.metaselash_2);
+            evol3.setImageResource(R.drawable.metaselash_3);
+            elmt1.setImageResource(R.drawable.bte_fire);
+            elmt2.setImageResource(R.drawable.bte_water);
+            skill.setImageResource(R.drawable.skill_metaselash);
+            nomMonstre.setText("Metaselash");
+
+        }else if(monsterId==4){
+            evol0.setImageResource(R.drawable.mechamancer_0);
+            evol1.setImageResource(R.drawable.mechamancer_1);
+            evol2.setImageResource(R.drawable.mechamancer_2);
+            evol3.setImageResource(R.drawable.mechamancer_3);
+            elmt1.setImageResource(R.drawable.bte_metal);
+            elmt2.setImageResource(R.drawable.bte_water);
+            skill.setImageResource(R.drawable.skill_mechamancer);
+            nomMonstre.setText("Mechamancer");
+
+        }else if(monsterId==5){
+            evol0.setImageResource(R.drawable.komocat_0);
+            evol1.setImageResource(R.drawable.komocat_1);
+            evol2.setImageResource(R.drawable.komocat_2);
+            evol3.setImageResource(R.drawable.komocat_3);
+            elmt1.setImageResource(R.drawable.bte_water);
+            elmt2.setImageResource(R.drawable.bte_nature);
+            skill.setImageResource(R.drawable.skill_komocat);
+            nomMonstre.setText("Komocat");
+
+        }else if(monsterId==6){
+            evol0.setImageResource(R.drawable.fenix_0);
+            evol1.setImageResource(R.drawable.fenix_1);
+            evol2.setImageResource(R.drawable.fenix_2);
+            evol3.setImageResource(R.drawable.fenix_3);
+            elmt1.setImageResource(R.drawable.bte_fire);
+            elmt2.setImageResource(R.drawable.bte_water);
+            skill.setImageResource(R.drawable.skill_fenix);
+            nomMonstre.setText("Fenix");
+
+        }else if(monsterId==7){
+            evol0.setImageResource(R.drawable.thunder_eagle_0);
+            evol1.setImageResource(R.drawable.thunder_eagle_1);
+            evol2.setImageResource(R.drawable.thunder_eagle_2);
+            evol3.setImageResource(R.drawable.thunder_eagle_3);
+            elmt1.setImageResource(R.drawable.bte_fire);
+            elmt2.setImageResource(R.drawable.bte_magic);
+            skill.setImageResource(R.drawable.skill_thunder_eagle);
+            nomMonstre.setText("Thunder Eagle");
+
+        }else if(monsterId==8){
+            evol0.setImageResource(R.drawable.pegasus_0);
+            evol1.setImageResource(R.drawable.pegasus_1);
+            evol2.setImageResource(R.drawable.pegasus_2);
+            evol3.setImageResource(R.drawable.pegasus_3);
+            elmt1.setImageResource(R.drawable.bte_nature);
+            elmt2.setImageResource(R.drawable.bte_light);
+            skill.setImageResource(R.drawable.skill_pegasus);
+            nomMonstre.setText("Pegasus");
+
+        }else if(monsterId==9){
+            evol0.setImageResource(R.drawable.obsidia_0);
+            evol1.setImageResource(R.drawable.obsidia_1);
+            evol2.setImageResource(R.drawable.obsidia_2);
+            evol3.setImageResource(R.drawable.obsidia_3);
+            elmt1.setImageResource(R.drawable.bte_earth);
+            elmt2.setImageResource(R.drawable.bte_dark);
+            skill.setImageResource(R.drawable.skill_obsidia);
+            nomMonstre.setText("Obsidia");
+
+        }else{}
+
+        // Flipper
         viewFlipper = (ViewFlipper) this.findViewById(R.id.bckgrndViewFlipper1);
-
-        fade_in = AnimationUtils.loadAnimation(this,
-                android.R.anim.fade_in);
-        fade_out = AnimationUtils.loadAnimation(this,
-                android.R.anim.fade_in);
-
+        fade_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        fade_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
         viewFlipper.setInAnimation(fade_in);
         viewFlipper.setOutAnimation(fade_out);
-
-
         viewFlipper.setAutoStart(true);
         viewFlipper.setFlipInterval(2000);
         viewFlipper.startFlipping();
+
     }
 }
--- app/src/main/java/com/example/wilder/monstermachine/MainActivity.java
+++ app/src/main/java/com/example/wilder/monstermachine/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.wilder.monstermachine;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,21 +14,100 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton btnSealion = (ImageButton) findViewById(R.id.imageSealion);
-        ImageButton btnChocoBunny = (ImageButton) findViewById(R.id.imageChocoBunny);
-        ImageButton btnPandaClaus = (ImageButton) findViewById(R.id.imagePandaClaus);
-        ImageButton btnMetaselash = (ImageButton) findViewById(R.id.imageMetaselash);
-        ImageButton btnMechamancer = (ImageButton) findViewById(R.id.imageMechamancer);
-        ImageButton btnKomocat = (ImageButton) findViewById(R.id.imageKomocat);
-        ImageButton btnFenix = (ImageButton) findViewById(R.id.imageFenix);
-        ImageButton btnThunderEagle = (ImageButton) findViewById(R.id.imageThunderEagle);
-        ImageButton btnPegasus = (ImageButton) findViewById(R.id.imagePegasus);
-        ImageButton btnObsidia = (ImageButton) findViewById(R.id.imageObsidia);
 
-        btnSealion.setOnClickListener(new View.OnClickListener() {
+        // Hide action bar
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
+
+        //
+        ImageButton btn0 = (ImageButton) findViewById(R.id.imageSealion);
+        ImageButton btn1 = (ImageButton) findViewById(R.id.imageChocoBunny);
+        ImageButton btn2 = (ImageButton) findViewById(R.id.imagePandaClaus);
+        ImageButton btn3 = (ImageButton) findViewById(R.id.imageMetaselash);
+        ImageButton btn4 = (ImageButton) findViewById(R.id.imageMechamancer);
+        ImageButton btn5 = (ImageButton) findViewById(R.id.imageKomocat);
+        ImageButton btn6 = (ImageButton) findViewById(R.id.imageFenix);
+        ImageButton btn7 = (ImageButton) findViewById(R.id.imageThunderEagle);
+        ImageButton btn8 = (ImageButton) findViewById(R.id.imagePegasus);
+        ImageButton btn9 = (ImageButton) findViewById(R.id.imageObsidia);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 0);
+                startActivity(intent);
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 1);
+                startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 2);
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 3);
+                startActivity(intent);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 4);
+                startActivity(intent);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 5);
+                startActivity(intent);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 6);
+                startActivity(intent);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 7);
+                startActivity(intent);
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 8);
+                startActivity(intent);
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,   DetailActivity.class);
+                intent.putExtra("monsterId", 9);
                 startActivity(intent);
             }
         });
